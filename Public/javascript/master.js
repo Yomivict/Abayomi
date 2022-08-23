2498ef801b462a871bc59702fb70ae07460abeba
Bamidele Emmanuel <abayomibamidele6@gmail.com>
2022-08-24 00:33:48 +1300

    pop

---
+++ Public/javascript/master.js
@@ -0,0 +1,11 @@
+function greet(first){
+    return first
+}
+console.log(greet('Hello world'))
+
+function anothergreet(second){
+    return second
+}
+console.log (anothergreet('hello'))
+console.log (anothergreet('bye-bye'))
+console.log (anothergreet('E kaale o'))
